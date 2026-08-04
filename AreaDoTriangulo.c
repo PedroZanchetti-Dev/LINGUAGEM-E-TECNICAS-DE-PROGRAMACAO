3850d27270a253f3ae0a357b4d883f762ea992d8
Pedro Zanchetti Souza <pedrozanc2008@gmail.com>
2026-08-04 08:54:52 -0300

    Create AreaDoTriangulo.c
    Exercício de cálculo de área de triângulo passado pelo Professor

---
+++ AreaDoTriangulo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Codigo para calcular area do triangulo*/
+
+int main() {
+	float base, altura, x;
+	
+	printf ("Digite o valor da Base: ");
+	 scanf("%f", &base);
+	 
+		
+	printf ("Digite o valor da Altura: ");
+	 scanf("%f", &altura);
+	 
+	 
+	x = (base * altura) /2;
+	
+	printf("A area do triangulo e: %f\n", x );
+	return 0;
+}
